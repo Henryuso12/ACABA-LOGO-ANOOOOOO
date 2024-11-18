998c58d6e972d52b639e32d6209efa905ad585e8
Tilambucano <169167580+Henryuso12@users.noreply.github.com>
2024-11-18 09:03:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Saúde',
+    'O que é obesidde?',
+    'É considerada é o excesso de gordura corporal, em quantidade que determine prejuízos à saúde. a pessoa que se mantém muito tempo inativa, sem praticar exercícios físicos'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Russia?',
+    'A capital da Russia é Moscou'
+)
+
+criaCartao(
+    'Espotes',
+    'O que é um Kung fu?',
+    'é a maneira ocidental de se referir à arte marcial de origem chinesa, o wushu.'
+)
+
+criaCartao(
+    'História',
+    'Quando foi criado o Basquete?',
+    'O Basquete foi criado em 1891'
+)
